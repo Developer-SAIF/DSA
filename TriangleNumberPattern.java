11adbb4056d57d00a79e407f176dbb720080ecf4
Developer-SAIF <639saifulislam@gmail.com>
2024-05-27 12:09:12 +0600

    Create TriangleNumberPattern.java

---
+++ TriangleNumberPattern.java
@@ -0,0 +1,25 @@
+/*
+# triangle number pattern.
+*/
+
+import java.util.Scanner;
+
+public class Main 
+{
+    public static void main (String [] args) 
+    {
+        Scanner sc = new Scanner (System.in);
+        
+        System.out.print ("Line numbers: ");
+        int line = sc.nextInt();
+        
+        for (int i = 1; i <= line; i++) 
+        {
+            for (int j = 1; j <= i; j++) 
+            {
+                System.out.print (j + " ");
+            }
+            System.out.println ("");
+        }
+    }
+}
