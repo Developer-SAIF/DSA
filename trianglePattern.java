5bfbcb906764f6ff8063f9e7980d4d5e59ed377f
Developer-SAIF <639saifulislam@gmail.com>
2024-05-27 11:59:38 +0600

    Create trianglePattern.java

---
+++ trianglePattern.java
@@ -0,0 +1,29 @@
+/*
+# triangle symbol pattern.
+*/
+
+import java.util.Scanner;
+
+public class Main 
+{
+
+    public static void main (String [] args) 
+    {
+        Scanner sc = new Scanner (System.in);
+
+        System.out.print ("Line numbers: ");
+        int line = sc.nextInt();
+        
+        System.out.print ("Symbol: ");
+        String symbol = sc.next();
+
+        for (int i = 1; i <= line; i++) 
+        {
+            for (int j = 1; j <= i; j++) 
+            {
+                System.out.print (symbol + " ");
+            }
+            System.out.println ("");
+        }
+    }
+}
